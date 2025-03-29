3ce096d02205457307a8f36502056e5980dd30c7
gustavo_CZ-tech <gustavo.tec1211@gmail.com>
2025-03-29 21:23:15 +0000

    Deesafio batalha naval (aventureiro)

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -6,36 +6,48 @@ void inicializartabuleiro(int tabuleiro[10][10]){
             tabuleiro[i][j] = 0;
         }
     }
+
 } 
 void posisaonavio(int tabuleiro[10][10]){
-    tabuleiro[2][4] = 3;
-    tabuleiro[2][5] = 3;
-    tabuleiro[2][6] = 3;
-    
-    tabuleiro[5][8] = 3;
-    tabuleiro[6][8] = 3;
-    tabuleiro[7][8] = 3;
-    
+    int i, j;
+
+    //navil horizontal
+    for(int i = 1; i < 4; i++){
+        tabuleiro[5][i] = 3;
+    }
+
+    //navil vertical
+    for(i = 1;i < 4; i++){
+        tabuleiro[i][5] = 3; 
+    }
+    for(i = 0; i < 3; i++ ){
+            tabuleiro[i][i] = 3;
+        }
+    for(i = 0; i < 3; i++){
+        tabuleiro[i][9 - i] = 3;
+    }
 }
+
 void exibirtabuleiro(int tabuleiro[10][10]){
 
     char letraposicao[10] = {'A','B','C','D','E','F','G','H','I','J'};
     
     printf("TABULEIRO DE BATALHA NAVAL \n\n");
-    printf("   ");
+    printf("  ");
     for(int i = 0; i < 10; i++){
         printf("%c ", letraposicao[i]);
      }
      printf("\n");
      
-     for(int i = 1; i < 10; i++ ){
+     for(int i = 0; i < 10; i++ ){
          printf("%d ", i);
          for(int j = 0; j < 10; j++){
              printf("%d ", tabuleiro[i][j]);
          }
          printf("\n");
      }
- }
+    printf("\n");
+}
 
 int main(){
     
