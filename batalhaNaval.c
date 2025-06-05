35022175ccca66578b9583e294c83034c6d44a57
gustavo_CZ <gustavo.tec1211@gmail.com>
2025-06-05 21:03:42 +0000

    desafio batalha naval nivel (MESTRE)

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,62 +1,75 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void inicializartabuleiro(int tabuleiro[10][10]){
-    for(int i = 0; i < 10; i++){
-        for(int j = 0; j < 10; j++){
+#define TAM 10
+
+void inicializarTabuleiro(int tabuleiro[TAM][TAM]) {
+    for (int i = 0; i < TAM; i++)
+        for (int j = 0; j < TAM; j++)
             tabuleiro[i][j] = 0;
-        }
-    }
-
-} 
-void posisaonavio(int tabuleiro[10][10]){
-    int i, j;
-
-    //navil horizontal
-    for(int i = 1; i < 4; i++){
-        tabuleiro[5][i] = 3;
-    }
-
-    //navil vertical
-    for(i = 1;i < 4; i++){
-        tabuleiro[i][5] = 3; 
-    }
-    for(i = 0; i < 3; i++ ){
-            tabuleiro[i][i] = 3;
-        }
-    for(i = 0; i < 3; i++){
-        tabuleiro[i][9 - i] = 3;
-    }
 }
 
-void exibirtabuleiro(int tabuleiro[10][10]){
+void exibirTabuleiro(int tabuleiro[TAM][TAM]) {
+    char letras[TAM] = {'A','B','C','D','E','F','G','H','I','J'};
 
-    char letraposicao[10] = {'A','B','C','D','E','F','G','H','I','J'};
-    
-    printf("TABULEIRO DE BATALHA NAVAL \n\n");
-    printf("  ");
-    for(int i = 0; i < 10; i++){
-        printf("%c ", letraposicao[i]);
-     }
-     printf("\n");
-     
-     for(int i = 0; i < 10; i++ ){
-         printf("%d ", i);
-         for(int j = 0; j < 10; j++){
-             printf("%d ", tabuleiro[i][j]);
-         }
-         printf("\n");
-     }
+    printf("   ");
+    for (int i = 0; i < TAM; i++)
+        printf("%c ", letras[i]);
+    printf("\n");
+
+    for (int i = 0; i < TAM; i++) {
+        printf("%2d ", i);
+        for (int j = 0; j < TAM; j++) {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
     printf("\n");
 }
 
-int main(){
-    
-    int tabuleiro[10][10];
+void habilidadeCruz(int tabuleiro[TAM][TAM], int x, int y, int alcance) {
+    for (int i = -alcance; i <= alcance; i++) {
+        if (x + i >= 0 && x + i < TAM)
+            tabuleiro[x + i][y] = 1;
+        if (y + i >= 0 && y + i < TAM)
+            tabuleiro[x][y + i] = 1;
+    }
+}
 
-    inicializartabuleiro(tabuleiro);
-    posisaonavio(tabuleiro);
-    exibirtabuleiro(tabuleiro);
+void habilidadeConeCima(int tabuleiro[TAM][TAM], int x, int y, int alcance) {
+    for (int i = 0; i <= alcance; i++) {
+        for (int j = -i; j <= i; j++) {
+            int novoX = x - i;
+            int novoY = y + j;
+            if (novoX >= 0 && novoY >= 0 && novoY < TAM)
+                tabuleiro[novoX][novoY] = 1;
+        }
+    }
+}
+
+void habilidadeOctaedro(int tabuleiro[TAM][TAM], int x, int y, int alcance) {
+    for (int i = 0; i < TAM; i++) {
+        for (int j = 0; j < TAM; j++) {
+            if (abs(i - x) + abs(j - y) <= alcance)
+                tabuleiro[i][j] = 1;
+        }
+    }
+}
+
+int main() {
+    int tabuleiro[TAM][TAM];
+    int x = 5, y = 5; // centro da habilidade
+    int alcance = 3;
+
+    inicializarTabuleiro(tabuleiro);
+
+    // Teste: ative só uma das habilidades por vez
+    // habilidadeCruz(tabuleiro, x, y, alcance);
+    // habilidadeConeCima(tabuleiro, x, y, alcance);
+    habilidadeOctaedro(tabuleiro, x, y, alcance);
+
+    exibirTabuleiro(tabuleiro);
 
     return 0;
-
 }
+
